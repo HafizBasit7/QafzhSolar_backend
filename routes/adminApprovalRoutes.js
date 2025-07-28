67cd8bd6a2c0247b684b955c258831e5f37f0f41
sudo-tahirsaeed <sudotahirsaeed@gmail.com>
2025-07-28 21:26:07 +0500

    renamed

--- routes/adminApprovalRoutes.js
+++ routes/adminApprovalRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const adminApprovalController = require('../controllers/adminApprovalController');
+const adminApprovalController = require('../controllers/AdminApprovalController');
 const { authToken, isAdmin } = require('../middlewares/auth');
 
 /**
